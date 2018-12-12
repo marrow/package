8c7ac3d2724228900fff7bbe460687a921553035
Alice Bevan-McGregor <alice@gothcandy.com>
2018-12-12 13:30:59 -0500

    Missing indentation.

--- marrow/package/tarjan.py
+++ marrow/package/tarjan.py
@@ -57,40 +57,40 @@ def topological_sort(graph):
 			count[successor] += 1
 
 	ready = [ node for node in graph if count[node] == 0 ]
-
+	
 	result = [ ]
 	while ready:
 		node = ready.pop(-1)
 		result.append(node)
-
+		
 		for successor in graph[node]:
 			count[successor] -= 1
 			if count[successor] == 0:
 				ready.append(successor)
-
+	
 	return result
 
 
 def robust_topological_sort(graph):
 	""" First identify strongly connected components,
 		then perform a topological sort on these components. """
-
+	
 	components = strongly_connected_components(graph)
-
+	
 	node_component = { }
 	for component in components:
 		for node in component:
 			node_component[node] = component
-
+	
 	component_graph = { }
 	for component in components:
 		component_graph[component] = [ ]
-
+	
 	for node in graph:
 		node_c = node_component[node]
 		for successor in graph[node]:
 			successor_c = node_component[successor]
 			if node_c != successor_c:
 				component_graph[node_c].append(successor_c) 
-
+	
 	return topological_sort(component_graph)
