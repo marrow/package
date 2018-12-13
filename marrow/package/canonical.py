6bdaec65e28eebd089bb82882401141cd98eeaa0
Alice Bevan-McGregor <alice@gothcandy.com>
2018-12-13 11:13:05 -0500

    Removed dead Python 2 support code for canonicalization.

--- marrow/package/canonical.py
+++ marrow/package/canonical.py
@@ -4,46 +4,6 @@ from typeguard import check_argument_types
 from typing import Callable
 
 
-def unwrap(obj:Callable, attr:str='__wrapped__') -> Callable:
-	"""Handle the @functools.wrap decorator protocol, determining the originally wrapped function."""
-	
-	assert check_argument_types()
-	
-	while hasattr(obj, attr):
-		obj = getattr(obj, attr)
-	
-	return obj
-
-
-def search(parent, obj, path:str='') -> str:
-	assert check_argument_types()
-	
-	obj = unwrap(unwrap(obj), '__func__')
-	
-	candidates = [
-			partial(parent.__dict__.get, obj.__name__, None),
-			partial(getattr, parent, obj.__name__, None)
-		]
-	
-	for candidate in candidates:
-		candidate = unwrap(unwrap(candidate()), '__func__')
-		if obj is candidate or obj == candidate:
-			return path + ('.' if path else '') + obj.__name__
-		
-	if path:
-		# We don't want to recurse forever... one level is acceptable for most applications.
-		# If you need deeper inspection, use Python 3.3+ for __qualname__ support.
-		raise LookupError("Can not identify canonical name for object: " + repr(obj))
-	
-	for name, member in getmembers(parent, isclass):
-		try:
-			return search(member, obj, name)
-		except LookupError:
-			pass
-	
-	raise LookupError("Can not identify canonical name for object: " + repr(obj))
-
-
 def name(obj) -> str:
 	"""This helper function attempts to resolve the dot-colon import path for a given object.
 	
@@ -56,18 +16,4 @@ def name(obj) -> str:
 	
 	module = getmodule(obj)
 	
-	try:
-		# Short-cut for Python 3.3+
-		return module.__name__ + ':' + obj.__qualname__
-	except AttributeError:
-		pass
-	
-	# This should handle all method combinations.
-	if hasattr(obj, '__func__'):
-		if hasattr(obj, '__self__') and isclass(obj.__self__):
-			return module.__name__ + ':' + search(module, obj.__self__) + '.' + obj.__name__
-		
-		obj = obj.__func__
-	
-	# Final hope.  Search.
-	return module.__name__ + ':' + search(module, obj)
+	return module.__name__ + ':' + obj.__qualname__
