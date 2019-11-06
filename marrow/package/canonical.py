5ef712d754f17fd6fd9dedc69249e35da1e22333
Alice Bevan-McGregor <alice@gothcandy.com>
2019-11-06 15:54:14 -0500

    Import cleanup, armour against getmodule lookup failure.

--- marrow/package/canonical.py
+++ marrow/package/canonical.py
@@ -1,19 +1,15 @@
-from functools import partial
-from inspect import getmodule, getmembers, isclass, isroutine
-from typeguard import check_argument_types
-from typing import Callable
+from inspect import getmodule, isroutine
 
 
 def name(obj) -> str:
-	"""This helper function attempts to resolve the dot-colon import path for a given object.
-	
-	Specifically searches for classes and methods, it should be able to find nearly anything at either the module
-	level or nested one level deep.  Uses ``__qualname__`` if available.
-	"""
+	"""Resolve the dot-colon import path for a given object as suitable for subsequent use with `lookup`."""
 	
 	if not isroutine(obj) and not hasattr(obj, '__name__') and hasattr(obj, '__class__'):
 		obj = obj.__class__
 	
 	module = getmodule(obj)
+	
+	if module is None:
+		raise LookupError("Unable to identify module for: " + repr(obj))
 	
 	return module.__name__ + ':' + obj.__qualname__
