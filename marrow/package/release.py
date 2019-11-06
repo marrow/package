d37881b4fab240cc328f351d42394680797baba7
Alice Bevan-McGregor <alice@gothcandy.com>
2019-11-06 15:55:36 -0500

    Version bump and release information architectural changes.

--- marrow/package/release.py
+++ marrow/package/release.py
@@ -3,10 +3,28 @@
 from collections import namedtuple
 
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 0, 1, 'final', 1)
-version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+class VersionInfo(namedtuple('VersionInfo', ('major', 'minor', 'micro', 'releaselevel', 'serial'))):
+	major:int
+	minor:int
+	micro:int
+	releaselevel:str
+	serial:int
+	
+	def __str__(self):
+		main = f"{self.major}.{self.minor}.{self.micro}"
+		supplement = f"{self.releaselevel[0]}{self.serial}"
+		return main if self.releaselevel == 'final' else (main + supplement)
 
-author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
+
+class Author(namedtuple('Author', ('name', 'email'))):
+	name:str
+	email:str
+
+
+version_info = VersionInfo(2, 0, 2, 'final', 1)
+version = str(version_info)
+
+author = Author("Alice Bevan-McGregor", 'alice@gothcandy.com')
 
 description = "A collection of utilities for resolving object names, names to objects, and managing plugins/extensions."
 url = 'https://github.com/marrow/package/'
