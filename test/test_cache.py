d4ebec570fe61821490af8edc5bf2595a524ec73
Alice Zoë Bevan–McGregor <amcgregor@cegid.com>
2022-11-29 12:03:20 -0500

    Test case modernization.

--- test/test_cache.py
+++ test/test_cache.py
@@ -4,22 +4,27 @@ from marrow.package import load, name, traverse
 from marrow.package.cache import PluginCache
 
 
-class TestPluginCache(TestCase):
-	candidates = ('py.test', 'coverage', 'pip')
+class TestCache:
+	pairs = (
+			('name', name),
+			('load', load),
+			('traverse', traverse),
+		)
 	
-	def test__cache__loads_expected_objects(self):
-		cache = PluginCache('console_scripts')
-		for candidate, obj in zip(self.candidates, (pytest, coverage, pip)):
-			assert cache[candidate] is obj
+	@pytest.mark.parametrize("name,obj", pairs)
+	def test__cache__loads_expected_objects(self, name, obj):
+		cache = PluginCache('marrow.package.sample')
+		assert cache[name] == obj
 	
 	def test__cache__attribute_access(self):
-		cache = PluginCache('console_scripts')
-		assert cache.coverage is coverage
+		cache = PluginCache('marrow.package.sample')
+		assert cache.traverse is traverse
 	
 	def test__cache__actually_caches_things(self):
-		cache = PluginCache('console_scripts')
+		cache = PluginCache('marrow.package.sample')
 		assert len(cache) == 0
-		assert 'coverage' not in cache
-		assert cache.coverage is coverage
+		assert 'load' not in cache
+		assert cache.load is load
 		assert len(cache) == 1
-		assert 'coverage' in cache
+		assert 'load' in cache
+
